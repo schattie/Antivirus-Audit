de3e025c78a44ee738d9c1cf2a1635217478f670
Hunter Schattie <schattieh@vanndata.com>
2023-02-27 16:51:29 -0500

    simple commit to make sure code is up-to-date

--- src/continuum.py
+++ src/continuum.py
@@ -24,4 +24,3 @@ class Continuum:
     def remove_cells_from_workbook(self): #used to remove all columns other than pc name
         self.sheet.delete_cols(1, 2)
         self.sheet.delete_cols(2, 13)
-       
