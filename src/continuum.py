d33814fd3e55becc2aea1c1b272d9e7a564a9872
Hunter Schattie <schattieh@vanndata.com>
2023-02-27 16:17:12 -0500

    Created a way to open excel files

--- src/continuum.py
+++ src/continuum.py
@@ -1,8 +1,26 @@
+from openpyxl import load_workbook
+from pathlib import Path
+
 class Continuum:
-    file_path = ""
+    file_string = None  #file path where excel sheet is stroed
+    file_path = None #This is the converted string value to path 
+    workstation_workbook = None #point this to path where file is stored
+    sheet = None #Should only be one sheet per excel, program auto selects first sheet. Used to manipulate sheet values. Mainly used to delete columns
 
-    def set_file_path(self, file_path):
-        self.file_path = file_path
+    def set_file_path(self, file_string): 
+        self.file_string = file_string
+        self.file_path = Path(self.file_string)
 
-    def get_file_path(self):
-        return str(self.file_path)
+    def get_file_path(self): 
+        return self.file_path
+
+    def get_file_string(self):
+        return self.file_string
+        
+    def load_workstation_workbook_and_sheet(self): #loads the workbook up and sets the first sheet to its working sheet
+        self.workstation_workbook = load_workbook(filename=self.file_path)
+        self.sheet = self.workstation_workbook.active
+
+    def remove_cells_from_workbook(self): #used to remove all columns other than pc name
+        self.sheet.delete_cols(1, 2)
+        self.sheet.delete_cols(4, 13)
--- src/test_continuum.py
+++ src/test_continuum.py
@@ -4,6 +4,10 @@ from continuum import Continuum
 class ContinuumTest(unittest.TestCase):
     continuum = Continuum()
     def test_file_path_set(self):
-        self.continuum.set_file_path("hello")
-        self.assertIn("hello", self.continuum.get_file_path())
-       
+        self.continuum.set_file_path("Antivirus-Audit\continuum-workstations\2023.xlsx")
+        self.assertIn("Antivirus-Audit\continuum-workstations\2023.xlsx", self.continuum.get_file_string())
+    
+    def test_excel_workbook_loading(self):
+        self.continuum.set_file_path("Z:/Antivirus-Audit/continuum-workstations/2023.xlsx")
+        self.continuum.load_workstation_workbook_and_sheet()
+        self.assertIn("REQ00000000000001192888", self.continuum.sheet.title)
