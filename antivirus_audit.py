9d617c4a1d5be56f00e43e8a9d170577654c5c4e
Hunter Schattie <schattieh@vanndata.com>
2023-03-07 15:11:55 -0500

    Succesfully communicated with API to get JWT token

--- antivirus_audit.py
+++ antivirus_audit.py
@@ -1,12 +1,13 @@
 from src.sophos import Sophos
+from decouple import config
 
 def main():
     sophos = Sophos()
+    url = "https://id.sophos.com/api/v2/oauth2/token"
 
     sophos.set_client_id()
     sophos.set_client_secret()
-    print(sophos.client_id)
-    print(sophos.client_secret)
+    print(sophos.authenticate_with_api(config('SOPHOS_CLIENT_ID'), config('SOPHOS_CLIENT_SECRET'), url))
 
 
 if __name__ == "__main__":
--- src/sophos.py
+++ src/sophos.py
@@ -20,7 +20,14 @@ class Sophos:
     def get_client_secret(self):
         return self.client_secret
     
-    def authenticate_with_api(self):
-        self.api_session.headers.update({'Content-Type:application/x-www-form-urlencoded'})
-        self.api_response = self.api_session.post('https://id.sophos.com/api/v2/oauth2/token', data = 'grant_type=client_credentials&client_id={self.client_id}&client_secret={self.client_secret}&scope=token')
-        print(self.api_response)
+    def authenticate_with_api(self, client_id, client_secret, url):
+        d = {
+            'grant_type': 'client_credentials',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'scope': 'token'
+        }
+        request_token = requests.post(url, auth=(client_id, client_secret), data=d)
+        json_token = request_token.json()
+        headers = {'Authorization': f"Bearer {json_token['access_token']}"}
+        return headers
