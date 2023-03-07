af8db3cb3a1a5e9dac7390f6c87fff6ad0cbf191
Hunter Schattie <schattieh@vanndata.com>
2023-03-07 15:00:06 -0500

    added key/secret to .env

--- antivirus_audit.py
+++ antivirus_audit.py
@@ -3,11 +3,11 @@ from src.sophos import Sophos
 def main():
     sophos = Sophos()
 
-    sophos.set_id_file_path("Z:\Antivirus-Audit\keys\sophos_client_id.txt")
-    sophos.set_secret_file_path("Z:\Antivirus-Audit\keys\sophos_client_secret.txt")
     sophos.set_client_id()
     sophos.set_client_secret()
-    sophos.authenticate_with_api()
+    print(sophos.client_id)
+    print(sophos.client_secret)
+
 
 if __name__ == "__main__":
     main()
--- src/sophos.py
+++ src/sophos.py
@@ -1,41 +1,18 @@
 from pathlib import Path
 import requests
+from decouple import config
 
 class Sophos:
-    id_file_string = None #file path where client id is stored
-    id_file_path = None #converts string into path 
-    secret_file_string = None
-    secret_file_path = None
     client_id = None 
     client_secret = None
     api_response = None
     api_session = requests.Session()
 
-    def set_id_file_path(self, id_file_string):
-        self.id_file_string = id_file_string
-        self.id_file_path = Path(self.id_file_string)
-
-    def set_secret_file_path(self, secret_file_string):
-        self.secret_file_string = secret_file_string
-        self.secret_file_path = Path(self.secret_file_string)
-
     def set_client_id(self):
-        self.client_id = Path(self.id_file_path).read_text()
+        self.client_id = config('SOPHOS_CLIENT_ID')
 
     def set_client_secret(self):
-        self.client_secret = Path(self.secret_file_path).read_text()
-
-    def get_id_file_path(self):
-        return self.id_file_path
-    
-    def get_secret_file_path(self):
-        return self.secret_file_path
-    
-    def get_id_file_string(self):
-        return self.id_file_string
-    
-    def get_secret_file_string(self):
-        return self.secret_file_string
+        self.client_secret = config('SOPHOS_CLIENT_SECRET')
     
     def get_client_id(self):
         return self.client_id
